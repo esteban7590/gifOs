bf557496d3ba2797329d66eca89768eed134b87c
esteban7590 <esteban7590@hotmail.com>
2020-04-12 20:44:23 -0500

    Theme change ready, dark styles missing

---
+++ js/theme.js
@@ -0,0 +1,9 @@
+let darkButton = document.getElementById("night");
+let lightButton = document.getElementById("day");
+
+darkButton.onclick = () => {
+  document.getElementsByClassName("sailor-day")[0].className = "sailor-night";
+};
+lightButton.onclick = () => {
+  document.getElementsByClassName("sailor-night")[0].className = "sailor-day";
+};
