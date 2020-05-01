e00e7101f67e7a89829cb7d508e779c3822ae60d
esteban7590 <esteban7590@hotmail.com>
2020-05-01 15:37:02 -0500

    Dropdown hides on window click

--- js/shared/theme-shared.js
+++ js/shared/theme-shared.js
@@ -1,21 +1,26 @@
-const DROPDOWN = document.getElementById("dropdown-themes");
+const dropdown = document.getElementById("dropdown-themes");
 
 const themeChange = () => {
-  document.getElementById("theme-button").onclick = () => {
-    console.log("drop");
-    DROPDOWN.style.display = "block";
+  document.getElementById("theme-button").onclick = (event) => {
+    dropdown.style.display = "block";
+    event.stopPropagation();
+    window.onclick = (ev) => {
+      dropdown.style.display = "none";
+    };
   };
-  document.getElementById("theme-button-arrow").onclick = () => {
-    DROPDOWN.style.display = "block";
+  document.getElementById("theme-button-arrow").onclick = (event) => {
+    dropdown.style.display = "block";
+    event.stopPropagation();
+    window.onclick = (ev) => {
+      dropdown.style.display = "none";
+    };
   };
   document.getElementById("night").onclick = () => {
-    console.log("night");
     document.getElementById("body-theme").className = "sailor-night";
-    DROPDOWN.style.display = "none";
+    dropdown.style.display = "none";
   };
   document.getElementById("day").onclick = () => {
-    console.log("day");
     document.getElementById("body-theme").className = "sailor-day";
-    DROPDOWN.style.display = "none";
+    dropdown.style.display = "none";
   };
 };
