b351f7b69fd92f5fe4f5458a2238cfb83521c666
esteban7590 <esteban7590@hotmail.com>
2020-08-16 11:59:22 -0500

    fixed bug caused by updated API endpoint url

--- js/shared/variables-shared.js
+++ js/shared/variables-shared.js
@@ -3,5 +3,5 @@ const API_KEY = "api_key=nviDGCCp3515X3VeiJdD4zAohJ9inqtJ";
 
 const TRENDING = API_URL + "/trending?" + API_KEY + "&limit=0";
 const SEARCH = API_URL + "/search?" + API_KEY + "&q=";
-const AUTOCOMPLETE = API_URL + "/search/tags?" + API_KEY + "&q=";
+const AUTOCOMPLETE = API_URL + "/search/tags?" + API_KEY + "&limit=3" + "&q=";
 const GET_BY_IDS = API_URL + "?" + API_KEY + "&" + "ids=";
