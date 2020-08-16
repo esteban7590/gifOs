6a7da61fa863f9be9b2ba11b4f6f770a351148d7
esteban7590 <esteban7590@hotmail.com>
2020-08-16 11:53:37 -0500

    updated an API endpoint url that was modified

--- js/shared/variables-shared.js
+++ js/shared/variables-shared.js
@@ -2,6 +2,6 @@ const API_URL = "https://api.giphy.com/v1/gifs";
 const API_KEY = "api_key=nviDGCCp3515X3VeiJdD4zAohJ9inqtJ";
 
 const TRENDING = API_URL + "/trending?" + API_KEY + "&limit=0";
-const SEARCH = API_URL + "/search?" + API_KEY + "&limit=0" + "&q=";
-const AUTOCOMPLETE = API_URL + "/search/tags?" + API_KEY + "&limit=3" + "&q=";
+const SEARCH = API_URL + "/search?" + API_KEY + "&q=";
+const AUTOCOMPLETE = API_URL + "/search/tags?" + API_KEY + "&q=";
 const GET_BY_IDS = API_URL + "?" + API_KEY + "&" + "ids=";
